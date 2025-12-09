7257435b9ed8c1f0e798e988a3a7bbdf9e84ceb5
Prithavi01 <prithaviap@gmail.com>
2025-12-09 20:29:38 +0530

    Added pattern11

---
+++ Basics/Logical Thinking/Pattern11.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printstar(int n){
+    int start=1;
+    for(int i=0;i<=n-1;i++){
+        if(i%2==0) start=1;
+        else start=0;
+        for(int j=0;j<=i;j++){
+            cout<<start;
+            start=1-start;
+        }
+    cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    printstar(n);
+    return 0;
+}

c78882f3b0d47fa8d69821017071daced3f804b6
Prithavi01 <prithaviap@gmail.com>
2025-12-09 21:51:21 +0530

    Added pattern13

--- Basics/Logical Thinking/Pattern13.cpp
+++ Basics/Logical Thinking/Pattern13.cpp
@@ -5,13 +5,10 @@ void printstar(int n){
     int count=1;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-            cout<<count;
+            cout<<count<<" ";
             count++;
-            }
-            }
-            else cout<<j;
         }
-    cout<<endl;
+        cout<<endl;
     }
 }
 int main(){

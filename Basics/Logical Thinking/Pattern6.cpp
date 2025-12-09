6ed2475603fe1be5bdb52e5737eaf09aebb42cbb
Prithavi01 <prithaviap@gmail.com>
2025-12-09 17:45:28 +0530

    Added Pattern6

---
+++ Basics/Logical Thinking/Pattern6.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printstar(int n){
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    printstar(n);
+    return 0;
+}

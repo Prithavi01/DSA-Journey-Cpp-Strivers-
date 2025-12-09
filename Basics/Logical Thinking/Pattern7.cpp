f87ee51baadb1f14ac15bb20664c2abff65b94df
Prithavi01 <prithaviap@gmail.com>
2025-12-09 18:15:28 +0530

    Added Pattern7

---
+++ Basics/Logical Thinking/Pattern7.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printstar(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=i*2-1;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    printstar(n);
+    return 0;
+}

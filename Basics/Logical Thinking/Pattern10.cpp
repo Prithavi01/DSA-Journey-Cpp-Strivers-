e6ad6e00af6ceec231aba7c2630f4ad1d44fe2ea
Prithavi01 <prithaviap@gmail.com>
2025-12-09 19:46:46 +0530

    Added Pattern9

---
+++ Basics/Logical Thinking/Pattern10.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printstar(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n) stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+                cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    printstar(n);
+    return 0;
+}

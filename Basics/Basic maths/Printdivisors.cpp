256ca39fb464ab8271b7c668e385eeb05ece5693
Prithavi01 <prithaviap@gmail.com>
2025-12-13 20:48:09 +0530

    Added printdivisors

---
+++ Basics/Basic maths/Printdivisors.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Printdivisors(int n){
+    vector<int>arr;
+    for(int i=1;i<=sqrt(n);i++){
+        if(n%i==0){
+            arr.push_back(i);
+            if(n/i!=i) arr.push_back(n/i);
+        } 
+    }
+    sort(arr.begin(),arr.end());
+    for(auto it:arr) {
+        cout<<(it)<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter N:";
+    cin>>n;
+    Printdivisors(n);
+}

3fa7d2dc8acd282879d3cfd9e4252fc1977601c6
Prithavi01 <prithaviap@gmail.com>
2025-12-22 12:00:28 +0530

    Added functional rec

---
+++ Basics/Basic Recursion/sumofnaturalnum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int SumofN(int n){
+    if(n==0){
+      return 0;
+    }
+    return n+SumofN(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter N:";
+    cin>>n;
+    int result=SumofN(n);
+    cout<<result;
+}
+//functional
+//Time complexity :O(n)
+//SPace complexity :O(n)
